70e27c30918c8dcb160256d96cca08d591d9a4a5
webmaster254 <josephm280@gmail.com>
2020-09-24 02:29:56 +0100

    added comment form

---
+++ frontend/src/components/UpvotesSection.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const UpvotesSection=({articleName,upvotes,setArticleInfo})=>{
+    const upvoteArticle= async ()=>{
+        const result= await fetch(`/api/articles/${articleName}/upvote`,{
+            methos:'post',
+        });
+        const body = await result.json();
+        setArticleInfo(body);
+    }
+    return(
+        <div id="upvote-section">
+            <button onClick={()=> upvoteArticle()}>Add Upvote</button>
+            <p>This post has been upvoted {upvotes} times</p>
+        </div>
+    );
+}
+
+export default UpvotesSection;
